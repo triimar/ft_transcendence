06dab8f4f22ba2daf1f19ceeece886ac6660df16
Maria Jose Travez <majotravez@gmail.com>
2024-08-14 10:43:46 +0200

    Frontend(feature): added logic for second player

--- transendence/static/js/game.js
+++ transendence/static/js/game.js
@@ -20,10 +20,24 @@ const ball = {
 };
 
 const paddleLeft= {
-	x: 10,
+	x: 0,
 	y: canvas.height/2,
 	vx: 0,
-	vy: 3,
+	vy: 6,
+	height: 100,
+	width: 50,
+	color: "green",
+	draw() {
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+};
+
+const paddleRight = {
+	x: canvas.width - 50,
+	y: canvas.height/2,
+	vx: 0,
+	vy: 6,
 	height: 100,
 	width: 50,
 	color: "green",
@@ -35,37 +49,58 @@ const paddleLeft= {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //ctx.fillRect(paddleLeft.x, paddleLeft.y, paddleLeft.width, paddleLeft.height);
   paddleLeft.draw();
+  paddleRight.draw();
   ball.draw();
   ball.x += ball.vx;
   ball.y += ball.vy;
   
-  if (
-    ball.y + ball.vy > canvas.height - ball.radius ||
-    ball.y + ball.vy < ball.radius
-) {
-	ball.vy = -ball.vy;
-}
-if (
-	ball.x + ball.vx > canvas.width - ball.radius ||
-    ball.x + ball.vx < ball.radius
-) {
-	ball.vx = -ball.vx;
-}
-
-raf = window.requestAnimationFrame(draw);
+  	if (
+		ball.y + ball.vy > canvas.height - ball.radius ||
+    	ball.y + ball.vy < ball.radius)
+	{
+		ball.vy = -ball.vy;
+	}
+	if (
+		ball.x + ball.vx > canvas.width - ball.radius ||
+		ball.x + ball.vx < ball.radius)
+	{
+		ball.vx = -ball.vx;
+	}
+	if (ball.x + ball.vx < ball.radius + paddleLeft.width &&
+		ball.y + ball.vy < paddleLeft.y + paddleLeft.height &&
+		ball.y + ball.vy > paddleLeft.y)
+	{
+		ball.vx = -ball.vx;
+	}
+	if (ball.x + ball.vx > paddleRight.x - ball.radius &&
+		ball.y + ball.vy < paddleRight.y + paddleRight.height &&
+		ball.y + ball.vy > paddleRight.y)
+	{
+		ball.vx = -ball.vx;
+	}
+	raf = window.requestAnimationFrame(draw);
 }
 
 document.addEventListener("keydown", (e) => {
 	console.log(paddleLeft.y);
 	switch (e.key) {
 		case "ArrowDown":
+			paddleRight.y += paddleRight.vy;
+			if (paddleRight.y > canvas.height - paddleRight.height/2)
+				paddleRight.y = canvas.height - paddleRight.height/2;
+		break;
+		case "ArrowUp":
+			paddleRight.y -= paddleRight.vy;
+			if (paddleRight.y < 0)
+				paddleRight.y = 0;
+			break;
+		case "s":
 			paddleLeft.y += paddleLeft.vy;
 			if (paddleLeft.y > canvas.height - paddleLeft.height/2)
 				paddleLeft.y = canvas.height - paddleLeft.height/2;
 		break;
-		case "ArrowUp":
+		case "w":
 			paddleLeft.y -= paddleLeft.vy;
 			if (paddleLeft.y < 0)
 				paddleLeft.y = 0;
