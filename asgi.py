2a6b9793bc1ba5fd0c227754d7c51350a3c89601
Triin <tmarts@student.42heilbronn.de>
2024-08-16 16:41:15 +0200

    Backend(feature): updates to settings and asgi

--- asgi.py
+++ asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from transcendence.consumers import YourConsumer #are we sure of the path? YourConsumer is not a name
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path('ws/some_path/', YourConsumer.as_asgi()), #needs modification
+        ])
+    ),
+})
