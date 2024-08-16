fdcbd5aa27ab4cda38cfaba2c89b353c596128f1
Triin <tmarts@student.42heilbronn.de>
2024-08-16 12:16:16 +0200

    Build(refractor): Fix misspelling of transcendence

--- transcendence/apps.py
+++ transcendence/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TransendenceConfig(AppConfig):
+class TranscendenceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transendence'
+    name = 'transcendence'
