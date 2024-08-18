e63ca899ed92872ab6efd2d307c5d7838d945731
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-08-18 15:25:22 +0200

    Frontend(chore): Avatar.js LobbyRoom.js Logo.js and NavigationBar.js use the corresponding template

--- transcendence/static/js/components/Avatar.js
+++ transcendence/static/js/components/Avatar.js
@@ -1,5 +1,13 @@
-export default ComponentAvatar extends HTMLElement {
+export default class ComponentAvatar extends HTMLElement {
+	static observedAttributes = ["avatar-name", "avatar-background"];
 	constructor() {
 		super();
+		const shadowRoot = this.attachShadow({ mode: "open" });
+		const template = document.getElementById("component-avatar");
+		shadowRoot.appendChild(template.content.cloneNode(true));
+	}
+
+	connectedCallback() {
+
 	}
 }
--- transcendence/static/js/components/NavigationBar.js
+++ transcendence/static/js/components/NavigationBar.js
@@ -1,5 +1,12 @@
-export default ComponentNavigationBar extends HTMLElement {
+export default class ComponentNavigationBar extends HTMLElement {
 	constructor() {
 		super();
+		const shadowRoot = this.attachShadow({ mode: "open" });
+		const template = document.getElementById("component-navigation-bar");
+		shadowRoot.appendChild(template.content.cloneNode(true));
+	}
+
+	connectedCallback() {
+
 	}
 }
