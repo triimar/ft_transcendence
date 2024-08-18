860e78327133244e281c6093110ff48dd5d3c01e
Sangyao42 <106156781+Sangyao42@users.noreply.github.com>
2024-08-18 19:04:28 +0200

    feat: Add Django admin

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+	path('admin/', admin.site.urls)
 ]
