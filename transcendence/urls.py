419feaa0a250f6d5618b133dc828f19696fa87fd
Triin Marts <triin.marts@gmail.com>
2024-08-22 12:01:57 +0200

    Backend(feature): added chat and room sites

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.transcendence, name='transcendence'),
-    path('hello_world', views.hello_world, name='hello_world')
+    path('hello_world', views.hello_world, name='hello_world'),
+    path('chat/', views.chat_lobby, name='chat_lobby'),
+    path('chat/<str:room_name>/', views.room, name='room'),
 ]
