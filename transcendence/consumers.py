a093294cb84b9ae38850dd618222a7110f367b78
Triin Marts <triin.marts@gmail.com>
2024-08-22 15:10:45 +0200

    Backend(feature): changed consumers.py to asyncronous websocket

--- transcendence/consumers.py
+++ transcendence/consumers.py
@@ -1,3 +1,41 @@
+# VER_3
+import json
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = "chat_%s" % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat_message", "message": message}
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
+
+# VER_1
 # import json
 
 # from channels.generic.websocket import WebsocketConsumer
@@ -16,44 +54,44 @@
 
 #         self.send(text_data=json.dumps({"message": message}))
 
+#VER_2
+# import json
 
-import json
-
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+# from asgiref.sync import async_to_sync
+# from channels.generic.websocket import WebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
+# class ChatConsumer(WebsocketConsumer):
+#     def connect(self):
+#         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+#         self.room_group_name = "chat_%s" % self.room_name
 
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+#         # Join room group
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.room_group_name, self.channel_name
+#         )
 
-        self.accept()
+#         self.accept()
 
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+#     def disconnect(self, close_code):
+#         # Leave room group
+#         async_to_sync(self.channel_layer.group_discard)(
+#             self.room_group_name, self.channel_name
+#         )
 
-    # Receive message from WebSocket
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+#     # Receive message from WebSocket
+#     def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json["message"]
 
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
-        )
+#         # Send message to room group
+#         async_to_sync(self.channel_layer.group_send)(
+#             self.room_group_name, {"type": "chat_message", "message": message}
+#         )
 
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event["message"]
+#     # Receive message from room group
+#     def chat_message(self, event):
+#         message = event["message"]
 
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+#         # Send message to WebSocket
+#         self.send(text_data=json.dumps({"message": message}))
