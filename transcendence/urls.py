b03490554bb940d16be54c481dd065447d4c86c9
Sangyao42 <106156781+Sangyao42@users.noreply.github.com>
2024-08-25 09:23:19 +0200

    chore

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-	  path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
     path('', views.transcendence, name='transcendence'),
     path('hello_world', views.hello_world, name='hello_world')
 ]
