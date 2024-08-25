55a2e107354cf20d6250f90e3b6a9d5cc0051b6d
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-08-25 17:28:29 +0200

    Frontend(chore): Single-paged game page

--- transcendence/views.py
+++ transcendence/views.py
@@ -6,9 +6,6 @@ from django.http import HttpResponse
 # def hello_world(request):
 #     return HttpResponse("Hello, world!")
 
-def hello_world(request):
-	return render(request, 'game.html')
-
 def transcendence(request):
     return render(request, 'index.html')
 
