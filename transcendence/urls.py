7125396a1d604d08b720dece07bc100fbe14b809
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-08-25 17:28:29 +0200

    Frontend(chore): Single-paged game page

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.transcendence, name='transcendence'),
-    path('hello_world', views.hello_world, name='hello_world'),
     path('chat/', views.chat_lobby, name='chat_lobby'),
     path('chat/<str:room_name>/', views.room, name='room'),
 ]
