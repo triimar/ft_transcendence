dc18b59832e441322848ea27b3a026de290274fa
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-08-25 17:30:22 +0200

    Frontend(chore): Remove hello_world

--- transcendence/static/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector("h1");
-    header.addEventListener("click", function() {
-        header.style.color = header.style.color === "red" ? "#2c3e50" : "red";
-    });
-});
--- transcendence/views.py
+++ transcendence/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-# def hello_world(request):
-#     return HttpResponse("Hello, world!")
-
 def transcendence(request):
     return render(request, 'index.html')
 
