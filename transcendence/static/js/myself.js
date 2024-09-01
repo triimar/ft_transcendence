f8c6278e9c34b84fa97433e58889e0907bccdc08
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-09-02 00:35:33 +0200

    Frontend(fix): Sending Websocket message before the connection is opened

--- transcendence/static/js/myself.js
+++ transcendence/static/js/myself.js
@@ -13,17 +13,63 @@ class Visitor {
 	}
 
 	connectWs() {
-		this.ws = new WebSocket('/');
+		this.ws = new WebSocket('/ws/transcendence/');
+		this.ws.addEventListener("open", function (event) {
+			console.log("Websocket connection is open");
+		});
 		this.ws.addEventListener("message", function (event) {
 			const message = JSON.parse(event.data);
+			console.log("Incoming message:", message);
 			switch (message.type) {
-				// TODO(HeiYiu): Receive message types
+				case "ack_init":
+					let pageHash = message.payload;
+					console.log(`client appeared on page ${pageHash}!`);
+				break;
 			}
 		});
 		this.ws.addEventListener("close", function (event) {
 			console.log("Websocket connection is closed unexpectedly");
 		});
 	}
+
+	#waitForOpenConnection() {
+		return new Promise((resolve, reject) => {
+			const maxNumberOfAttempts = 10;
+			const intervalTime = 200; //ms
+
+			let currentAttempt = 0;
+			const interval = setInterval(() => {
+				if (currentAttempt > maxNumberOfAttempts - 1) {
+					clearInterval(interval);
+					reject(new Error('Maximum number of attempts exceeded'));
+				} else if (this.ws.readyState === this.ws.OPEN) {
+					clearInterval(interval);
+					resolve();
+				}
+				currentAttempt++;
+			}, intervalTime);
+		});
+	}
+
+	async sendMessage(message) {
+		if (this.ws.readyState !== this.ws.OPEN) {
+			try {
+				await this.#waitForOpenConnection();
+				this.ws.send(message);
+			} catch (err) { console.error(err); }
+		} else {
+			this.ws.send(message);
+		}
+	}
+
+	async sendMessageInit(pageHash) {
+		let message = {
+			type: "init",
+			payload: pageHash
+		};
+		this.sendMessage(JSON.stringify(message));
+	}
+
 }
 
 export const myself = new Visitor();
