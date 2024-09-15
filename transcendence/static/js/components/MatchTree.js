f544fab2fe697b5fb5f89cb9ae5b234a57a7b5ab
Triin Marts <triin.marts@gmail.com>
2024-09-15 16:34:50 +0200

    Frontennd(feature): added mock json content in the component file

--- transcendence/static/js/components/MatchTree.js
+++ transcendence/static/js/components/MatchTree.js
@@ -1,5 +1,6 @@
 export default class ComponentMatchTree extends HTMLElement {
 	//static observedAttributes = ["room-max", "room-id"];
+	
 	constructor() {
 		super();
 		this.shadow = this.attachShadow({ mode: "open" });
@@ -7,8 +8,50 @@ export default class ComponentMatchTree extends HTMLElement {
 		this.shadow.appendChild(template.content.cloneNode(true));
 	}
 
-	attributeChangedCallback(name, oldValue, newValue) {}
-
 	connectedCallback() {
+		
 	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+
+	}
+
 }
+
+//#input json
+// {
+//   "matchlist-avatars": [
+//     {
+//       "avatar-name": "A.A",
+//       "avatar-background": "#FF5733"
+//     },
+//     {
+//       "avatar-name": "pip",
+//       "avatar-background": "#33FF57"
+//     },
+//     {
+//       "avatar-name": "wmw",
+//       "avatar-background": "#3357FF"
+//     },
+//     {
+//       "avatar-name": "0)0",
+//       "avatar-background": "#FF33A6"
+//     },
+//     {
+//       "avatar-name": "E9E",
+//       "avatar-background": "#FFD133"
+//     },
+//     {
+//       "avatar-name": "T.T",
+//       "avatar-background": "#A633FF"
+//     },
+//     {
+//       "avatar-name": "$u$",
+//       "avatar-background": "#33FFF5"
+//     },
+//     {
+//       "avatar-name": "HuH",
+//       "avatar-background": "#FF8333"
+//     }
+//   ]
+// }
