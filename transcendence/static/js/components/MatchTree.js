b369163761d9d33a42ff0f3b8da8f76787f4cb5c
Triin Marts <triin.marts@gmail.com>
2024-09-15 18:09:16 +0200

    Frontend(feature): added fetchMatchData() to componet

--- transcendence/static/js/components/MatchTree.js
+++ transcendence/static/js/components/MatchTree.js
@@ -8,50 +8,23 @@ export default class ComponentMatchTree extends HTMLElement {
 		this.shadow.appendChild(template.content.cloneNode(true));
 	}
 
-	connectedCallback() {
-		
+	fetchMatchData() {
+        return {
+            "matchlist-avatars": [
+                { "avatar-name": "A.A", "avatar-background": "#FF5733" },
+                { "avatar-name": "pip", "avatar-background": "#33FF57" },
+                { "avatar-name": "wmw", "avatar-background": "#3357FF" },
+                { "avatar-name": "0)0", "avatar-background": "#FF33A6" },
+                { "avatar-name": "E9E", "avatar-background": "#FFD133" },
+                { "avatar-name": "T.T", "avatar-background": "#A633FF" },
+                { "avatar-name": "$u$", "avatar-background": "#33FFF5" },
+                { "avatar-name": "HuH", "avatar-background": "#FF8333" }
+            ]
+        };
 	}
-
-	attributeChangedCallback(name, oldValue, newValue) {
-
+	connectedCallback() {
+	
 	}
 
 }
 
-//#input json
-// {
-//   "matchlist-avatars": [
-//     {
-//       "avatar-name": "A.A",
-//       "avatar-background": "#FF5733"
-//     },
-//     {
-//       "avatar-name": "pip",
-//       "avatar-background": "#33FF57"
-//     },
-//     {
-//       "avatar-name": "wmw",
-//       "avatar-background": "#3357FF"
-//     },
-//     {
-//       "avatar-name": "0)0",
-//       "avatar-background": "#FF33A6"
-//     },
-//     {
-//       "avatar-name": "E9E",
-//       "avatar-background": "#FFD133"
-//     },
-//     {
-//       "avatar-name": "T.T",
-//       "avatar-background": "#A633FF"
-//     },
-//     {
-//       "avatar-name": "$u$",
-//       "avatar-background": "#33FFF5"
-//     },
-//     {
-//       "avatar-name": "HuH",
-//       "avatar-background": "#FF8333"
-//     }
-//   ]
-// }
