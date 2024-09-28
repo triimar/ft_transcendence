5fc5bc72feefa46921801391f57aaa635220b038
Eunsol Kim <eunsol.code@gmail.com>
2024-09-28 16:40:50 +0200

    Backend(feat): basic OAuth authentification working

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
+from oauth2_provider import urls as oauth2_urls
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,6 @@ urlpatterns = [
     path('', views.transcendence, name='transcendence'),
     path('chat/', views.chat_lobby, name='chat_lobby'),
     path('chat/<str:room_name>/', views.room, name='room'),
+    path('trigger_auth/', views.oauth_redirect, name='oauth_redirect'),
+    path('authentification/', views.oauth_callback, name='oauth_callback'),
 ]
