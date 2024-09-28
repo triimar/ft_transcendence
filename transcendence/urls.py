9fe21e240d41401fed06dd2c37f483813d9ce360
Eunsol Kim <eunsol.code@gmail.com>
2024-09-28 17:55:28 +0200

    Backend(fix): changed paths of auth redirection

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from django.contrib import admin
-from oauth2_provider import urls as oauth2_urls
 from . import views
 
 urlpatterns = [
@@ -8,6 +7,7 @@ urlpatterns = [
     path('', views.transcendence, name='transcendence'),
     path('chat/', views.chat_lobby, name='chat_lobby'),
     path('chat/<str:room_name>/', views.room, name='room'),
-    path('trigger_auth/', views.oauth_redirect, name='oauth_redirect'),
-    path('authentification/', views.oauth_callback, name='oauth_callback'),
+    path('api/trigger_auth/', views.oauth_redirect, name='oauth_redirect'),
+    path('api/auth_request/', views.oauth_callback, name='oauth_callback'),
+    path('check_auth/', views.check_auth, name='check_auth'),
 ]
