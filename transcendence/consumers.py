8f89fd5a305a495b89d9c755a396bb16aa11a028
Maria Jose Travez <majotravez@gmail.com>
2024-10-12 16:12:35 +0200

    Added routing and websocket for the gameboard

--- transcendence/consumers.py
+++ transcendence/consumers.py
@@ -27,6 +27,39 @@ class TranscendenceConsumer(AsyncWebsocketConsumer):
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"type": "ack_init", "payload": message}))
 
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'game_{self.room_name}'
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'game_message',
+                'message': message
+            }
+        )
+
+    async def game_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
 messages = []
 
 class ChatConsumer(AsyncWebsocketConsumer):
--- transcendence/routing.py
+++ transcendence/routing.py
@@ -4,5 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+	re_path(r'ws/game/(?P<room_name>\w+)/$', consumers.GameConsumer.as_asgi()),
     path("ws/transcendence/", consumers.TranscendenceConsumer.as_asgi()),
 ]
