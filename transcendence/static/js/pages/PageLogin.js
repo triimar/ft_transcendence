1c343927b148f7b0de67213d81a259a24e7ea6fa
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-10-19 23:38:36 +0200

    frontend(fix): login buttons should work now

--- transcendence/static/js/pages/PageLogin.js
+++ transcendence/static/js/pages/PageLogin.js
@@ -9,10 +9,16 @@ export default class PageLogin {
         this.btnLoginGuestEvent = async () => {
             console.log("guest login");
             await myself.login(true);
+            let lastPageHash = localStorage.getItem("last_page_hash");
+            if (!lastPageHash) lastPageHash = "main";
+            location.hash = '#' + lastPageHash;
+            localStorage.removeItem("last_page_hash");
         };
         this.container.querySelector("#btn-login-guest").addEventListener("click", this.btnLoginGuestEvent);
         this.btnLoginIntraEvent = async () => {
             console.log("intra login");
+            let lastPageHash = localStorage.getItem("last_page_hash");
+            if (!lastPageHash) localStorage.setItem("last_page_hash", "main");
             await myself.login(false);
         };
         this.container.querySelector("#btn-login-intra").addEventListener("click", this.btnLoginIntraEvent);
