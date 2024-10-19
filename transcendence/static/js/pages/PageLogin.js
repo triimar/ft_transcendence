23f671659a17664ab4491430a11b723acaee3150
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-10-19 21:32:10 +0200

    frontend(feature): Login button call /api/login_guest/ or /api/trigger_auth/

--- transcendence/static/js/pages/PageLogin.js
+++ transcendence/static/js/pages/PageLogin.js
@@ -1,3 +1,4 @@
+import { myself } from "../myself.js"
 export default class PageLogin {
 	constructor(container) {
 		this.templateId = "page-login";
@@ -5,10 +6,21 @@ export default class PageLogin {
 	}
 
 	attachEvents() {
-
+        this.btnLoginGuestEvent = async () => {
+            console.log("guest login");
+            await myself.login(true);
+        };
+        this.container.querySelector("#btn-login-guest").addEventListener("click", this.btnLoginGuestEvent);
+        this.btnLoginIntraEvent = async () => {
+            console.log("intra login");
+            await myself.login(false);
+        };
+        this.container.querySelector("#btn-login-intra").addEventListener("click", this.btnLoginIntraEvent);
 	}
 
 	removeEvents() {
-
+        // Note(HeiYiu): One might not need to removeEventListener because the tree will be removed anyway
+        // this.container.querySelector("#btn-login-guest").removeEventListener("click", this.#btnLoginGuestEvent);
+        // this.container.querySelector("#btn-login-intra").removeEventListener("click", this.#btnLoginIntraEvent);
 	}
 }
