e1bcc00d7212aff61be3e73ebebcd2cc2b1835bb
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-10-19 21:29:09 +0200

    backend(refactor): import modules explicitly

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from django.contrib import admin
 from . import views
+from . import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.transcendence, name='transcendence'),
     path('chat/', views.chat_lobby, name='chat_lobby'),
     path('chat/<str:room_name>/', views.room, name='room'),
-    path('api/trigger_auth/', views.oauth_redirect, name='oauth_redirect'),
-    path('callback/', views.oauth_callback, name='oauth_callback'),
+    path('api/trigger_auth/', api.oauth_redirect, name='oauth_redirect'),
+    path('callback/', api.oauth_callback, name='oauth_callback'),
     # path('api/auth_request/', views.oauth_callback, name='oauth_callback'),
-    path('api/check_auth/', views.check_auth, name='check_auth'),
-    path('api/guest_login/', views.guest_login, name='guest_login'),
-    path('api/logout/', views.logout, name='logout'),
+    path('api/check_auth/', api.check_auth, name='check_auth'),
+    path('api/guest_login/', api.guest_login, name='guest_login'),
+    path('api/logout/', api.logout, name='logout'),
 ]
