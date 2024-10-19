bad6f451568866d2daf51a3d8dc7a31bf9f45795
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-10-19 23:38:15 +0200

    frontend(chore): show a label when intra login is successful

--- transcendence/static/js/pages/PageMain.js
+++ transcendence/static/js/pages/PageMain.js
@@ -1,3 +1,4 @@
+import { myself } from "../myself.js"
 export default class PageMain {
 	constructor(container) {
 		this.templateId = "page-main";
@@ -5,7 +6,21 @@ export default class PageMain {
 	}
 
 	attachEvents() {
-
+        if (myself.getLoginMethod() == "intra")
+        {
+            let label = document.createElement("p");
+            label.textContent = "Intra User";
+            label.style.color = "black";
+            label.style.fontSize = "15px";
+            label.style.background = "yellow";
+            label.style.border = "2px solid black";
+            label.style.zIndex = "5";
+            label.style.position = "fixed";
+            label.style.width = "fit-content";
+            label.style.top = "0";
+            label.style.left = "0";
+            this.container.appendChild(label);
+        }
 	}
 
 	removeEvents() {
