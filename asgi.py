3888c4cd753f131487e395d756fe1931de971d13
Sangyao42 <106156781+Sangyao42@users.noreply.github.com>
2024-10-27 15:35:54 +0100

    fix: import errors

--- asgi.py
+++ asgi.py
@@ -4,7 +4,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from .transcendence.redis_client import _redis_client
+from transcendence.redis_client import _redis_client
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transcendence.settings")
 
