000019930646b6ad781367f91a38258b1e2200d0
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-10-27 02:41:18 +0200

    frontend(feature): working popup window for navigatiob bar

--- transcendence/static/js/components/NavigationBar.js
+++ transcendence/static/js/components/NavigationBar.js
@@ -14,9 +14,17 @@ export default class ComponentNavigationBar extends HTMLElement {
 			document.documentElement.style.setProperty('--td-ui-font-color', bgColor);
 		}).bind(this);
 		this.shadow.querySelector("#mode").addEventListener("click", this.modeSwitcherFunc, true);
+
+        this.toggleAvatarInfoFunc = (() => {
+            this.shadow.querySelector("#avatar-info-container").classList.toggle("hide");
+        }).bind(this);
+		this.shadow.querySelector("#avatar").addEventListener("click", this.toggleAvatarInfoFunc, true);
+		this.shadow.querySelector("#close-btn").addEventListener("click", this.toggleAvatarInfoFunc, true);
 	}
 
 	disconnectedCallback() {
 		this.shadow.querySelector("#mode").removeEventListener("click", this.modeSwitcherFunc, true);
+		this.shadow.querySelector("#avatar").removeEventListener("click", this.toggleAvatarInfoFunc, true);
+		this.shadow.querySelector("#close-btn").removeEventListener("click", this.toggleAvatarInfoFunc, true);
 	}
 }
