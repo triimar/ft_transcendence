c556783456065a99ef85fa15240acf103d003560
Eunsol Kim <eunsol.code@gmail.com>
2024-10-27 16:27:38 +0100

    Backend(fix): import only needed functions, jwt for guest

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
 from django.contrib import admin
 from . import views
-from . import api
+from .api import guest_login, oauth_redirect, oauth_callback, check_auth, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.transcendence, name='transcendence'),
     path('chat/', views.chat_lobby, name='chat_lobby'),
     path('chat/<str:room_name>/', views.room, name='room'),
-    path('api/trigger_auth/', api.oauth_redirect, name='oauth_redirect'),
-    path('api/callback/', api.oauth_callback, name='oauth_callback'),
-    path('api/check_auth/', api.check_auth, name='check_auth'),
-    path('api/guest_login/', api.guest_login, name='guest_login'),
-    path('api/logout/', api.logout, name='logout'),
+    path('api/trigger_auth/', oauth_redirect, name='oauth_redirect'),
+    path('api/callback/', oauth_callback, name='oauth_callback'),
+    path('api/check_auth/', check_auth, name='check_auth'),
+    path('api/guest_login/', guest_login, name='guest_login'),
+    path('api/logout/', logout, name='logout'),
 ]
