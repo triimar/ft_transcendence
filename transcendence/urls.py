c1159bdf17e57af04d512a16f021653c69bda133
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-11-04 04:12:50 +0100

    Backend(feature): Add api endpoint to get avatar information

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
 from . import views
-from .api import guest_login, oauth_redirect, oauth_callback, check_auth, logout
+from .api import guest_login, oauth_redirect, oauth_callback, check_auth, avatar_information, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('api/callback/', oauth_callback, name='oauth_callback'),
     path('api/check_auth/', check_auth, name='check_auth'),
     path('api/guest_login/', guest_login, name='guest_login'),
+    path('api/myself/', avatar_information, name='avatar_information'),
     path('api/logout/', logout, name='logout'),
 ]
