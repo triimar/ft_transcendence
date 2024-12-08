f29b100fa727e2758180a0c6b37109b56c8c47b5
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-12-08 15:58:10 +0100

    frontend(refactor): cat all pages into the same js file

--- transcendence/static/js/index.js
+++ transcendence/static/js/index.js
@@ -8,13 +8,7 @@ import ComponentRoomSettingSize from "./components/RoomSettingSize.js";
 import ComponentButton from "./components/Button.js";
 import ComponentLever from "./components/Lever.js";
 import ComponentTournamentTree from "./components/TournamentTree.js";
-
-import PageError from "./pages/PageError.js";
-import PageLogin from "./pages/PageLogin.js";
-import PageGame from "./pages/PageGame.js";
-import PageAiGame from "./pages/PageAiGame.js";
-import PageRoom from "./pages/PageRoom.js";
-import PageMain from "./pages/PageMain.js";
+import { PageError, PageLogin, PageGame, PageAiGame, PageRoom, PageMain } from "./pages.js";
 
 import { myself } from "./myself.js";
 
---
+++ transcendence/static/js/pages.js
@@ -0,0 +1,137 @@
+import { myself } from "../myself.js"
+export class PageAiGame {
+	constructor(container) {
+		this.templateId = "page-ai-game";
+		this.container = container;
+	}
+
+	attachEvents() {
+
+	}
+
+	removeEvents() {
+
+	}
+}
+
+export class PageError {
+	constructor(container) {
+		this.templateId = "page-error";
+		this.container = container;
+	}
+
+	attachEvents() {
+
+	}
+
+	removeEvents() {
+
+	}
+}
+
+export class PageGame {
+	constructor(container) {
+		this.templateId = "page-game";
+		this.container = container;
+	}
+
+	attachEvents() {
+
+	}
+
+	removeEvents() {
+
+	}
+}
+
+export class PageLogin {
+	constructor(container) {
+	this.templateId = "page-login";
+	this.container = container;
+	}
+
+	attachEvents() {
+		this.btnLoginGuestEvent = async () => {
+			console.log("guest login");
+			let is_success = await myself.login(true);
+			if (!is_success) {
+				localStorage.removeItem("login_method");
+				location.hash = "#login";
+				window.dispatchEvent(new HashChangeEvent("hashchange"));
+			}
+			else {
+				let hash = window.location.hash.slice(1);
+				if ((hash == '') || (hash == "login")) {
+					hash = "main";
+					location.hash = '#' + hash;
+				} else {
+					location.hash = '#' + hash;
+					window.dispatchEvent(new HashChangeEvent("hashchange"));
+				}
+			}
+		};
+		this.container.querySelector("#btn-login-guest").addEventListener("click", this.btnLoginGuestEvent);
+		this.btnLoginIntraEvent = async () => {
+			console.log("intra login");
+			let lastPageHash = localStorage.getItem("last_page_hash");
+			if (!lastPageHash) localStorage.setItem("last_page_hash", "main");
+			await myself.login(false);
+		};
+		this.container.querySelector("#btn-login-intra").addEventListener("click", this.btnLoginIntraEvent);
+	}
+
+	removeEvents() {
+		// Note(HeiYiu): One might not need to removeEventListener because the tree will be removed anyway
+		// this.container.querySelector("#btn-login-guest").removeEventListener("click", this.#btnLoginGuestEvent);
+		// this.container.querySelector("#btn-login-intra").removeEventListener("click", this.#btnLoginIntraEvent);
+	}
+}
+
+export class PageMain {
+	constructor(container) {
+	this.templateId = "page-main";
+	this.container = container;
+	}
+
+	attachEvents() {
+		if (myself.getLoginMethod() == "intra")
+		{
+			let label = document.createElement("p");
+			label.textContent = "Intra User";
+			label.style.color = "black";
+			label.style.fontSize = "15px";
+			label.style.background = "yellow";
+			label.style.border = "2px solid black";
+			label.style.zIndex = "5";
+			label.style.position = "fixed";
+			label.style.width = "fit-content";
+			label.style.top = "0";
+			label.style.left = "0";
+			this.container.appendChild(label);
+		}
+		let addRoomButton = this.container.querySelector("#add-room-btn");
+		addRoomButton.addEventListener("click", myself.sendMessageAddRoom.bind(myself));
+	}
+
+	removeEvents() {
+
+	}
+}
+
+export class PageRoom {
+	constructor(container) {
+		this.templateId = "page-room";
+		this.container = container;
+	}
+
+	attachEvents() {
+		let leaveRoomButton = this.container.querySelector("#leave-room-btn");	
+		this.leaveRoomFunc = () => {
+			myself.sendMessageLeaveRoom(myself.roomId);
+		};
+		leaveRoomButton.addEventListener("click", this.leaveRoomFunc);
+	}
+
+	removeEvents() {
+	}
+}
