192d1ddc9a7d9ba06fa7f839dc2d2aa7d4d9849a
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-12-21 17:38:54 +0100

    backend(refactor): remove redundant files
    One can always get back the files from history using git checkout
    <branchname> -- <path_to_file>

--- transcendence/urls.py
+++ transcendence/urls.py
@@ -6,8 +6,6 @@ from .api import guest_login, oauth_redirect, oauth_callback, check_auth, avatar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.transcendence, name='transcendence'),
-    path('chat/', views.chat_lobby, name='chat_lobby'),
-    path('chat/<str:room_name>/', views.room, name='room'),
     path('api/trigger_auth/', oauth_redirect, name='oauth_redirect'),
     path('api/callback/', oauth_callback, name='oauth_callback'),
     path('api/check_auth/', check_auth, name='check_auth'),
