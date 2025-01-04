fee9b8dfd5a3577a567e9bf4754b64235e349f67
Triin Marts <triin.marts@gmail.com>
2025-01-04 18:16:44 +0100

    frontend(Feature): translations for index.html strings done

--- transcendence/static/js/translation.js
+++ transcendence/static/js/translation.js
@@ -36,21 +36,58 @@ export function updateGlobalTranslations() {
     if (!isI18nInitialized) return; // Ensure i18n is initialized
 
     const footerAuthors = document.getElementById("footer-authors");
-    if (footerAuthors) {
+	const addRoom = document.getElementById("add-room");
+	const exitRoom = document.getElementById("exit-room"); 	
+	const tree = document.getElementById("tree-btn-txt");
+	const exitGame = document.getElementById("exit-game");
+	const pageError = document.getElementById("page-error-txt");
+    if (footerAuthors)
         footerAuthors.textContent = i18next.t("footer.authors");
-    }
+	if (addRoom)
+		addRoom.textContent = i18next.t("add-room");
+	if (exitRoom)
+		exitRoom.textContent = i18next.t("exit-room");
+	if (tree)
+		tree.textContent = i18next.t("tree-btn-txt");
+	if (exitGame) 
+		exitGame.textContent = i18next.t("exit-game");
+	if (pageError)
+		pageError.textContent = i18next.t("page-error-txt");
 
     updateLoginTranslations();
+	updatePopupTranslations();
+	updateLobbyTranslations();
 }
 
 function updateLoginTranslations() {
     const loginGuest = document.getElementById("login-guest");
     const loginIntra = document.getElementById("login-intra");
-
-    if (loginGuest) {
+    if (loginGuest) 
         loginGuest.textContent = i18next.t("login.guest");
-    }
-    if (loginIntra) {
+    if (loginIntra) 
         loginIntra.textContent = i18next.t("login.intra");
-    }
+}
+
+function updateLobbyTranslations() {
+	const prepare = document.getElementById("prepare-btn-txt");
+	if (prepare)
+		prepare.textContent = i18next.t("lobby-room.prepare-btn-txt")
+}
+
+function updatePopupTranslations() {
+	const customAvatar = document.getElementById("custom-avatar");
+	const logout = document.getElementById("logout")
+	const confirmLogout = document.getElementById("confirm-logout-txt")
+	const agree = document.getElementById("agree-txt")
+	const decline = document.getElementById("decline-txt")
+	if (customAvatar)
+		customAvatar.textContent = i18next.t("popups.custom-avatar")
+	if (logout)
+		logout.textContent = i18next.t("popups.logout");
+	if (confirmLogout)
+		confirmLogout.textContent = i18next.t("popups.confirm-logout-txt");
+	if (agree)
+		agree.textContent = i18next.t("popups.agree-txt");
+	if (decline)
+		decline.textContent = i18next.t("popups.decline-txt");
 }
