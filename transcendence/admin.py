56ddee9fe0d394acfc0cd455201ea1c5845be9db
Eunsol Kim <eunsol.code@gmail.com>
2025-01-05 19:26:46 +0100

    Backend(feat): create a table through model migration done

--- transcendence/admin.py
+++ transcendence/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-# from . import models
+from django.db import models
 
-# admin.site.register(models.Player)
+admin.site.register(models.Users)
--- transcendence/models.py
+++ transcendence/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-
-# Create your models here.
-
-# class Player(models.Model):
-# 	pass
+from django.core.validators import RegexValidator
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)  # Automatically creates the SERIAL column
+    uuid = models.CharField(max_length=22)
+    login = models.CharField(max_length=10)
+    avatar = models.CharField(max_length=3)
+    color = models.CharField(
+        max_length=6,
+        validators=[RegexValidator(regex=r'^[A-F0-9]{6}$', message="Invalid color format.")]
+    )
