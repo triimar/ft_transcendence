c67ed5e7c433d3cc40ce762fbb40379a5f6bb1d6
Triin Marts <triin.marts@gmail.com>
2025-01-12 20:19:40 +0100

    Frontend(feature): focus on popup and page navigation

--- transcendence/static/js/components/Lever.js
+++ transcendence/static/js/components/Lever.js
@@ -6,7 +6,18 @@ export default class ComponentLever extends HTMLElement {
 		this.shadow.appendChild(template.content.cloneNode(true));
 		this.shadow.host.addEventListener("click", () => {
 			let handleElement = this.shadow.querySelector("#handle");
-			handleElement.classList.toggle("on");
+			const isOn = handleElement.classList.toggle("on");
+			this.setAttribute("aria-checked", isOn ? "true" : "false");
+		});
+	}
+
+	connectedCallback() {
+		this.setAttribute("role", "switch");
+		this.setAttribute("tabindex", "0")
+		this.addEventListener("keydown", (e) => {
+			if ((e.key === "Enter") || (e.key === " ")) {
+				this.click();
+			}
 		});
 	}
 }
