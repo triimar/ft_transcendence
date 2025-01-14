3391091f7b8fafde32fe3b142280434044b7b778
Eunsol Kim <eunsol.code@gmail.com>
2025-01-14 03:01:54 +0100

    Backend(fix): deleted migration and wsgi, collect static files by call_command, tested websocket functionality except the game, tested if lifespan startup and shutdown routines work with ctrl c and docker compose stop, tested db connection and queries with new server

--- asgi.py
+++ asgi.py
@@ -1,10 +1,12 @@
 import os
 import django
+from time import sleep
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from django.core.management import call_command
 from transcendence.redis_client import _redis_client
 from transcendence.db_async_queries import add_user
 
@@ -18,6 +20,14 @@ import transcendence.routing
 
 # Initialize Django application
 django.setup()
+
+def collect_static_files():
+    try:
+        # Run Django's collectstatic command
+        call_command('collectstatic', verbosity=1, interactive=False)
+        print("Static files collected successfully.")
+    except Exception as e:
+        print(f"Error collecting static files: {e}")
 
 # Define lifespan scope (startup and shutdown)
 async def lifespan_scope(scope, receive, send):
@@ -33,6 +43,7 @@ async def lifespan_scope(scope, receive, send):
                 
                 print("Application is starting up...")
                 print("hi")
+                collect_static_files()
                 # Perform any startup tasks (e.g., connect to Redis, etc.)
                 # Example: await _redis_client.connect() if needed
                 await send({'type': 'lifespan.startup.complete'})
@@ -40,8 +51,9 @@ async def lifespan_scope(scope, receive, send):
             elif message['type'] == 'lifespan.shutdown':
                 print("Application is shutting down...")
                 # Perform cleanup tasks (e.g., close Redis connections)
-                await add_user("123", "test", "tst", "tttt")
-                await _redis_client.close()  # Clean up Redis connection pool
+                await add_user("126", "test_user4", "d_d", "BCC933")
+                print("bye")
+                # await _redis_client.close()  # Clean up Redis connection pool
                 await send({'type': 'lifespan.shutdown.complete'})
                 return  # Exit the lifespan loop after shutdown
 
--- transcendence/admin.py
+++ transcendence/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from . import models
+from .tests import models
 
 admin.site.register(models.Users)
