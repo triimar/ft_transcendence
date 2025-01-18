2655fba76dadf3088965332dcbd5fea5f1b64523
Eunsol Kim <eunskim@student.42heilbronn.de>
2025-01-18 15:49:44 +0100

    Backend(fix): closing redis works now

--- asgi.py
+++ asgi.py
@@ -7,6 +7,9 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.core.management import call_command
 from transcendence.db_connection import init_db_connection, close_asyncpg_pool
+from transcendence.db_async_queries import select_all_users
+from transcendence.redis_client import get_redis_client
+from transcendence.redis_data import add_one_player
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
@@ -20,44 +23,46 @@ import transcendence.routing
 django.setup()
 
 def collect_static_files():
-    try:
-        # Run Django's collectstatic command
-        call_command('collectstatic', verbosity=1, interactive=False)
-        print("Static files collected successfully.")
-    except Exception as e:
-        print(f"Error collecting static files: {e}")
+		try:
+				# Run Django's collectstatic command
+				call_command('collectstatic', verbosity=1, interactive=False)
+				print("Static files collected successfully.")
+		except Exception as e:
+				print(f"Error collecting static files: {e}")
 
 # Define lifespan scope (startup and shutdown)
 async def lifespan_scope(scope, receive, send):
-    """
-    Handle application lifespan events
-    """
+		"""
+		Handle application lifespan events
+		"""
 
-    if scope['type'] == 'lifespan':
-        while True:
-            message = await receive()
+		if scope['type'] == 'lifespan':
+				while True:
+						message = await receive()
 
-            if message['type'] == 'lifespan.startup':
-                print("[lifespan] Application is starting up...")
-                collect_static_files()
-                await init_db_connection()
-                # TODO: sync db to redis
-                await send({'type': 'lifespan.startup.complete'})
-            
-            elif message['type'] == 'lifespan.shutdown':
-                print("[lifespan] Application is shutting down...")
-                # TODO: sync redis to db
-                # TODO: how to clean up redis connections (should I?)
-                await close_asyncpg_pool()
-                await send({'type': 'lifespan.shutdown.complete'})
-                return  # exit the lifespan loop after shutdown
+						if message['type'] == 'lifespan.startup':
+								print("[lifespan] Application is starting up...")
+								collect_static_files()
+								await init_db_connection()
+								# TODO: sync db to redis
+								await select_all_users()
+								await send({'type': 'lifespan.startup.complete'})
+						
+						elif message['type'] == 'lifespan.shutdown':
+								print("[lifespan] Application is shutting down...")
+								# TODO: sync redis to db
+								await get_redis_client().close()
+								print("Redis connection closed")
+								await close_asyncpg_pool()
+								await send({'type': 'lifespan.shutdown.complete'})
+								return  # exit the lifespan loop after shutdown
 
 application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(transcendence.routing.websocket_urlpatterns))
-        ),
-        "lifespan": lifespan_scope,
-    }
+		{
+				"http": django_asgi_app,
+				"websocket": AllowedHostsOriginValidator(
+						AuthMiddlewareStack(URLRouter(transcendence.routing.websocket_urlpatterns))
+				),
+				"lifespan": lifespan_scope,
+		}
 )
