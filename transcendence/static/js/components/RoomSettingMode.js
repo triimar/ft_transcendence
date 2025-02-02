983b384c538c567cf7f43bdf95d26814bf47f08d
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2025-02-02 19:49:16 +0100

    frontend(feat): Add RoomSettingMode component

--- transcendence/static/js/components/RoomSettingMode.js
+++ transcendence/static/js/components/RoomSettingMode.js
@@ -8,36 +8,61 @@ export default class ComponentRoomSettingMode extends HTMLElement {
 	}
 
 	connectedCallback() {
-		// const incButton = this.shadow.querySelector("#inc-button");
-		// const decButton = this.shadow.querySelector("#dec-button");
-		// incButton.addEventListener("click", () => {
-		// 	if (this.size < this.maxSize) {
-		// 		this.size++;
-		// 		this.shadow.querySelector("#label").textContent = i18next.t("label.people", { count: this.size });
-		// 		if (this.size == this.maxSize) {
-		// 			incButton.setAttribute("disabled", "");
-		// 		}
-		// 		else {
-		// 			incButton.removeAttribute("disabled");
-		// 			decButton.removeAttribute("disabled");
-		// 		}
-		// 	}
-		// });
-		// decButton.addEventListener("click", () => {
-		// 	if (this.size > this.minSize) {
-		// 		this.size--;
-		// 		this.shadow.querySelector("#label").textContent = i18next.t("label.people", { count: this.size });
-		// 		if (this.size == this.minSize) {
-		// 			decButton.setAttribute("disabled", "");
-		// 		}
-		// 		else {
-		// 			decButton.removeAttribute("disabled");
-		// 			incButton.removeAttribute("disabled");
-		// 		}
-		// 	}
-		// });
-		// if (this.size == this.minSize) decButton.setAttribute("disabled", "");
-		// if (this.size == this.maxSize) incButton.setAttribute("disabled", "");
-		// this.shadow.querySelector("#label").textContent = i18next.t("label.people", { count: this.size });
+		let container = this.shadow.querySelector("#mode-btn-container");
+		container.addEventListener("click", (event) => {
+			for (let button of container.children) {
+				if (event.target == button) {
+					let id = event.target.id;
+					let modeName = id.substring(0, id.indexOf("-"));
+					button.classList.add("chosen");
+					this.setAttribute("room-mode", modeName);
+				} else {
+					button.classList.remove("chosen");
+				}
+			}
+		});
+		let description = this.shadow.querySelector("#game-mode-description-txt");
+		container.addEventListener("mouseover", (event) => {
+			let id = event.target.id;
+			switch (id) {
+			case "classic-mode-btn": {
+				description.textContent = i18next.t("classic-mode-description");
+			} break;
+			case "balance-mode-btn": {
+				description.textContent = i18next.t("balance-mode-description");
+			} break;
+			case "local-mode-btn": {
+				description.textContent = i18next.t("local-mode-description");
+			} break;
+			}
+		});
+		this.setAttribute("room-mode", "classic");
+		container.addEventListener("mouseout", (event) => {
+			let modeName = this.getAttribute("room-mode");
+			description.textContent = i18next.t(`${modeName}-mode-description`);
+		});
+		this.shadow.querySelector("#classic-mode-btn-txt").textContent = i18next.t("classic-mode-txt");
+		this.shadow.querySelector("#balance-mode-btn-txt").textContent = i18next.t("balance-mode-txt");
+		this.shadow.querySelector("#local-mode-btn-txt").textContent = i18next.t("local-mode-txt");
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		let description = this.shadow.querySelector("#game-mode-description-txt");
+		if (name == "room-mode") {
+			switch (newValue) {
+			case "classic": {
+				this.shadow.querySelector("#classic-mode-btn").click();
+				description.textContent = i18next.t("classic-mode-description");
+			} break;
+			case "balance": {
+				this.shadow.querySelector("#balance-mode-btn").click();
+				description.textContent = i18next.t("balance-mode-description");
+			} break;
+			case "local": {
+				this.shadow.querySelector("#local-mode-btn").click();
+				description.textContent = i18next.t("local-mode-description");
+			} break;
+			}
+		}
 	}
 }
