d1a640fdf2a3ff54fc80bd86d04b7c4f9a5741c7
Sangyao42 <wangsangyao@hotmail.com>
2025-02-16 14:44:45 +0100

    backend(fix): only reset player score before the match starts

--- transcendence/redis_data.py
+++ transcendence/redis_data.py
@@ -356,13 +356,16 @@ async def generate_matches(room_id, self_player_id) -> list[str]:
 async def set_player_ready_for_match(room_id, match_index, player_id):
     redis_instance = get_redis_client()
 
+    match_status = check_match_status(room_id, match_id=match_index)
+    if match_status == MatchStatus.BEFORE_START:
+        await redis_instance.json().set("player_data", f"$.{player_id}.score", 0)
+
     await redis_instance.json().set(
         "room_data", f"$.{room_id}.matches[{match_index}].started", True)
 
     await redis_instance.json().numincrby(
         "room_data", f"$.{room_id}.matches[{match_index}].ready", 1
     )
-    await redis_instance.json().set("player_data", f"$.{player_id}.score", 0)
 
 async def set_match_ready_to_zero(room_id, match_index):
     redis_instance = get_redis_client()
